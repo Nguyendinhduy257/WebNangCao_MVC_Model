d24ffbe56b32efe75ab5d3bc8ddb4c57adc7a1ef
Nguyendinhduy257 <Nguyendinhduy257@gmail.com>
2026-02-14 15:10:11 +0700

    thêm thư bảng User vào PostGreSQL từ Code First

--- WebNangCao_MVC_Model/Data/AppDbContext.cs
+++ WebNangCao_MVC_Model/Data/AppDbContext.cs
@@ -8,6 +8,6 @@ namespace WebNangCao_MVC_Model.Data
         {
             // --- KHAI BÁO CÁC BẢNG (TABLES) ---
             //Đang xem trên Code trên Mermaid Diagram
-        }
+        public DbSet<User> Users { get; set; } // Tương ứng với bảng "Users" trong Database
     }
 }
