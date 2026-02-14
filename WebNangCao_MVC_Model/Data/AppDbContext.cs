a8b4eac51781e52427d08b042fef16d13211264d
Nguyendinhduy257 <Nguyendinhduy257@gmail.com>
2026-02-14 18:52:35 +0700

    Cập nhật thư mục Fluent API

--- WebNangCao_MVC_Model/Data/AppDbContext.cs
+++ WebNangCao_MVC_Model/Data/AppDbContext.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 using WebNangCao_MVC_Model.Models;
+using WebNangCao_MVC_Model.Data.Fluent_API; // Import namespace chứa UserConfiguration
 namespace WebNangCao_MVC_Model.Data
 {
     public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            // --- KHAI BÁO CÁC BẢNG (TABLES) ---
-            //Đang xem trên Code trên Mermaid Diagram
+        {   
+        }
+        // --- KHAI BÁO CÁC BẢNG (TABLES) ---
+        //Đang xem trên Code trên Mermaid Diagram
         public DbSet<User> Users { get; set; } // Tương ứng với bảng "Users" trong Database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // CÁCH 1: Kích hoạt thủ công từng file (Dùng cách này để dễ kiểm soát)
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            // CÁCH 2: Kích hoạt TẤT CẢ file cấu hình trong project (Nhanh, gọn)
+            // modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
     }
 }
--- WebNangCao_MVC_Model/Models/User.cs
+++ WebNangCao_MVC_Model/Models/User.cs
@@ -1,6 +1,24 @@
-﻿namespace WebNangCao_MVC_Model.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebNangCao_MVC_Model.Models
 {
+    [Table("Users")] // Tên bảng trong Database
     public class User
     {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty; // Tương ứng với Register.Name
+
+        public string Username { get; set; } = string.Empty;// Tương ứng với Register.Username
+
+        public string Email { get; set; } = string.Empty;    // Tương ứng với Register.Email
+
+        public string PasswordHash { get; set; } = string.Empty; // Tương ứng với Register.Password (Đã mã hóa)
+
+        public string Role { get; set; } = "student"; // Tương ứng với Register.Role
+
+        // Thêm trường này để quản lý thời gian tạo
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
